07b1e3da49348ff95e0fd2e8023e36639e28303d
Andrii Khan <andrii.khan@studysmarter.de>
2023-11-07 23:25:11 +0100

    added google markup for FAQ

--- inc/Gutenberg/Blocks/Faq/Block.php
+++ inc/Gutenberg/Blocks/Faq/Block.php
@@ -14,6 +14,17 @@ class Block extends Gutenberg_Block {
 	public function init_fields() {
 
 		$this->add_field( [
+			'label'         => __( 'Google Markup', 'fp' ),
+			'name'          => 'google_markup',
+			'type'          => 'button_group',
+			'choices'       => [
+				'yes' => __( 'Yes', 'fp' ),
+				'no'  => __( 'No', 'fp' ),
+			],
+			'default_value' => 'no',
+		] );
+
+		$this->add_field( [
 			'label'        => __( 'FAQ', 'fp' ),
 			'name'         => 'faq',
 			'type'         => 'repeater',
